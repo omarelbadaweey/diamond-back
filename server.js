9a7cab6803394823519591101f4f3dfbc1182cc9
Omar Elbadaweey <omarelbadawey10@icloud.com>
2026-06-29 16:41:03 +0300

    edit index route

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ require("dotenv").config()
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the API');
+});
+
 app.use( '/api',router)
 const PORT = process.env.PORT || 5000;
 
